9d1b96bcf0a305afb92ec9df9244d0c907a69506
as0808 <67689138+as0808@users.noreply.github.com>
2021-10-05 12:12:30 +0530

    Update code.py

--- cars/code.py
+++ cars/code.py
@@ -4,20 +4,20 @@ import numpy as np
 cars=pd.read_csv("/cars.csv")
 cars
 
-X1=cars.iloc[:,2:4].values
-X1
+x=cars.iloc[:,2:4].values
+x
 
-y1=cars.iloc[:,-1].values
-y1
+y=cars.iloc[:,-1].values
+y
 
 from sklearn.model_selection import train_test_split
-X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1, test_size = 0.2, random_state =2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state =2)
 
 from sklearn.ensemble import RandomForestRegressor
 model1 = RandomForestRegressor()
 
-model1.fit(X1_train,y1_train)
+model1.fit(x_train,y_train)
 
-model1.predict(X1_test)
+model1.predict(x_test)
 
-model1.score(X1_test,y1_test)
+model1.score(x_test,y_test)
